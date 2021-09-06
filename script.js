08df02508c545f097c6543bb39d43f434e881497
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-06 13:52:18 +0000

    implement simple tests

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const inputData = ["stwórz", "sobie", "tutaj", "więcej", "wyrazów", "byłem", "byłem", "wczoraj", "w", "domu", "i", "leżałem", "bardzo", "długo",];
 
 const findPhraseInArray = (array, phrase) => {
+
+  if (!Array.isArray(array)) throw new Error('Podaj tablicę!');
+  if (typeof phrase != 'string') throw new Error('Podaj stringa');
+
+  const arrayCopy = [...array];
+
+  // for (const index in arrayCopy){
+
+  // }
+
   // return
 };
 
