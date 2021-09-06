ff20dadc9035bb595b598a09ff2712a7ded1d194
MichaPotoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-06 13:47:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputData = ["stwórz", "sobie", "tutaj", "więcej", "wyrazów", "byłem", "byłem", "wczoraj", "w", "domu", "i", "leżałem", "bardzo", "długo",];
+
+const findPhraseInArray = (array, phrase) => {
+  // return
+};
+
+
+
+
+
+// przykładowe działanie:
+const result = findPhraseInArray(inputData, "tut");
+// result === [ [2, 'tutaj'], [5, 'TUTAJ'] ]
